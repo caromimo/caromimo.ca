7c9fff605669b405e74b3c211698aa31a014aadb
Caroline Mimeault <carolinemimeault@gmail.com>
2024-12-28 20:25:06 -0500

    migrate the recipes

---
+++ migrations/000002_add_recipes.up.sql
@@ -0,0 +1,104 @@
+INSERT INTO recipes (title, ingredients, instructions, source, tags) 
+VALUES ('Braised Pork with Sauerkraut and Apples', 
+'3-4 pound pork loin or shoulder, bone-in,
+1 pound bag of sauerkraut,
+2 large apple, thinly sliced,
+2 large onion, thinly sliced,
+2 tablespoons coconut sugar,
+salt and pepper to season',
+'Preheat the oven to 325*F
+In a large Dutch oven or casserole dish, layer half of the onions and apples in the bottom.
+Place pork, fat side up, on the onions and apples. Season with salt and pepper.
+Pile the sauerkraut on top of the pork and press a bit to help it stay on top. Pour any remaining liquid from the bag into the dish. Sprinkle the coconut sugar over the sauerkraut.
+Add the remaining onions and apples around the pork. Cover with a lid (or foil), and braise in the oven for 3-4 hours (about an hour per pound of pork) until the pork is tender and ready to shred.
+Remove from the oven and increase oven temperature to 425*F.
+While the oven is coming to temp, shred the pork with tongs or two forks. Mix the meat with the onions, apples and sauerkraut until evenly distributed.
+Return the meat, uncovered, to the oven and roast for another 20-30 minutes until the ends are golden brown but not dried out.
+Serve over mashed potatoes and enjoy!', 
+'https://www.instagram.com/reel/DCFjmkgOPyy/', 
+ARRAY ['diner', 'meat']
+);
+
+INSERT INTO recipes (title, ingredients, instructions, source, tags) 
+VALUES ('Overnight Focaccia', 
+'500 g white bread flour, 12% protein content or higher,
+420 ml water, slightly warm,
+1 teaspoon instant yeast,
+1 teaspoon honey,
+1 tablespoon extra virgin olive oil,
+2 teaspoons fine sea salt',
+'Step 1
+Please see the notes below the recipe about the importance of using bread flour for this recipe and how to adapt the recipe for plain/all purpose flour if bread flour isn’t available.
+Day One - Dough Mixing Day
+Step 2
+In a large mixing bowl, stir together the slightly warm water, instant yeast, honey, extra virgin olive oil and fine sea salt until evenly combined. Tip your bread flour into the bowl with the rest of the ingredients and mix with a spoon until all dry flour patches have disappeared. Place cloth over top of bowl, let rest for 10 minutes at room temperature.
+Step 3
+After 10 minutes, you will perform a stretch + fold. This helps to develop the gluten in the dough which will help with the structure of the bread. Wet your hand (this prevents dough from sticking to you), grab a handful of dough from the 12 o’clock position, pull it up slightly and then pull it all the way up and over the bulk of the dough to the 6 o’clock position. Repeat this action on all sides of the dough until it feels like you’ve created a bit tension and you can’t stretch the dough up and over anymore…it should have tightened up into a rough ball. If the dough is wetter/stickier than mine is (in video tutorial I’ve linked) and doesn’t form into a ball, don’t worry too much! It’s just that all bread flours absorb different amounts of water. It should bake up just fine the following day! Let rest for 10 minutes and then repeat the stretch + fold action one more time.
+Step 4
+After the second set of stretch and folds, wet your hands, gather your dough and place it seam side down into bowl so the top is quite smooth (this helps prevent air bubbles from escaping).
+Step 5
+Top the dough with a good drizzle of olive oil (1 Tbsp) and rub it over the dough to make sure entire top surface has been covered. Cover your bowl with a reusable shower cap, a lid, plastic wrap, or transfer into a Tupperware container. The dough will rise in the fridge during its rest, so make sure there is room in the bowl for growth. Put in coldest spot of your fridge and let rest in there for 12 - 48 hours.
+Day Two - Baking Day
+Step 6
+Prepare your 9” x 13” (22cm x 33cm) baking tray. Put a tiny bit of oil on the tray, rub it all around and then place a sheet of parchment paper on top and press down. The oil helps the parchment to stick in place. Then drizzle about 2-3 tablespoons of olive oil on the parchment paper and spread it evenly around the base and sides of the paper lining the tray/pan. The parchment prevents the focaccia from sticking to the tray when baking.
+Step 7
+Remove your dough from fridge and using a curved dough scraper, gently release the dough from the sides of the bowl and tip the dough into your oiled and lined tray.
+Step 8
+Now, you are going to oil your hands and fold one side of dough towards the middle of the blob of dough. Repeat with the other side, a bit like folding a piece of paper into thirds. Then flip the dough so the seams from the folding are at the bottom and the smooth side is at the top.
+Step 9
+Cover using another tray, inverted - this prevents the dough from drying out and forming a crust while it rises. If you don’t have another tray, a plastic storage box can work too. Do not cover the dough with plastic wrap or a tea towel, it will stick.
+Step 10
+Let sit for about 1.5 - 2.5 hours (the dough should be mostly filling the space in the tray). If it’s not filling the space after this period of time, oil your hands and from the underside of the dough you gently pull it towards the edges of the tray. The timing will depend on the temperature on the day. If it’s a very hot day it might only take 1.5 hours to puff up/spread in tray. On a very cold day it could take 2.5 hours.
+Dimpling, Topping + Baking Your Focaccia
+Step 11
+Once you think the dough is ready (it will be floofy and jiggly when you shake the tray), it’s time to preheat your oven to 220C/430F.
+imgOvenPreheat,220°C
+Step 12
+Drizzle the top of the dough with a little more extra virgin olive oil and then oil your hands. Using both hands, press your fingers into the dough, gently touching the bottom of the tray. Repeat until the entire tray of dough is dimpled. There is such a thing as too much dimpling though…you can lose some of the nice bubbles you’ve created in the dough if you dimple too much. Just be mindful of air pockets and try not to pop them…these bubbles are what make the crumb of a focaccia so interesting and delicious.
+Step 13
+Top with a sprinkle of flaky sea salt. Add anything you’d like to the top by gently pressing toppings into the little dimples: olives, rosemary (coat in a bit of oil so it doesn’t burn), cherry tomatoes, onions, jalapeños + cheese. Before dimpling, you could top with basil pesto or with my chimichurri sauce if using oily toppings like these, then omit the drizzle of olive oil.
+Step 14
+I always bake my focaccia for about 18-22 minutes in a 220C/430F oven. I bake it on the lowest rack in the oven because I think it helps to crisp the bottom of focaccia and prevent the top from burning. If you have a pizza setting in your oven, use that; the main heat source will come from the bottom, which I think is ideal for baking focaccia. Check the focaccia after 18-20 minutes (it might still need more time!) and remove from oven when it’s reached a deep golden brown colour. I’ve learned that every oven is so different so don’t worry about overbaking it if it takes closer to 25-28 (or more) minutes in your oven. The interior will still remain soft! If this is the case and it takes on the longer side, even up to 35 mins, I’d recommend upping your oven temp for the next time (turn up by 10-20 degrees), it could just be your oven runs a little on the cooler side.
+Step 15
+I let it cool for a couple of minutes in the pan and then I transfer it to a cooling rack so the bottom doesn’t steam while sitting in the tray. I want it to stay nice and crispy.
+Step 16
+You can top with more olive oil when it’s come out of the oven if you wish for it to look burnished and glistening. If I’ve topped the focaccia with pesto or chimichurri I don’t tend to do this. With other toppings I do a little drizzle and it soaks right into the crust when it’s hot out of the oven.
+Step 17
+Let cool for at least 15-20 minutes before cutting in!
+', 
+'https://www.instagram.com/reel/CstmeLIICOl/', 
+ARRAY ['bread']
+);
+
+INSERT INTO recipes (title, ingredients, instructions, source, tags) 
+VALUES ('Homemade Chicken Shawarma', 
+'2.5 lbs of boneless chicken thighs,
+2-3 tablespoons olive oil, 
+3-4 pinches of salt, 
+fresh cracked pepper, 
+2 tbsp of plain yogurt, 
+the juice of 1/2 large lemon,
+1 tbsp thyme, 
+1.5 tbsp garlic powder, 
+1 tbsp smoked paprika, 
+1/2 tbsp Aleppo pepper, 
+1/2 tsp cayenne pepper, 
+1/2 tsp turmeric, 
+1/2 tsp cumin, 
+1/4 tsp cardamom, 
+1/2 tsp cinnamon, 
+1/8 tsp nutmeg,
+1/2 red onion.
+',
+'Mix all ingredients together. 
+Marinate from 2 hours to 2 days.
+Remove from the fridge.
+Preheat the oven to 425*F.
+Pack the marinated chicken tightly into a bread loaf pan (about 8.5x4.5x2.5). 
+Pop it into the oven for 45-55 mins at 425F.
+Remove the chicken from the oven, tilt the pan over a glass bowl or container to pour out the excess juices (save the juice to use as broth or to pour over the chicken).
+Flip the loaf pan onto a cutting board to remove the chicken and cut it into thin slices or strips.
+Serve it up however way you want - as is, on top of a salad, on a power bowl, whatever! This stays well in the fridge in an airtight lid for 4-5 days.', 
+'https://www.instagram.com/shredhappens/reel/C2yTnDMuaRk/?hl=en', 
+ARRAY ['diner', 'meat']
+);
